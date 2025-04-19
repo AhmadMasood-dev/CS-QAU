bf5be1178de7ef9b453b55b661c7a844324a0a08
AhmadMasood-dev <ahmadmasood.dev@gmail.com>
2025-04-20 00:27:38 +0500

    project setup

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+
+// // /** @type {import('tailwindcss').Config} */
+// module.exports = {
+//   content: ["./src/**/*.{js,jsx,ts,tsx}"],
+//   theme: {
+//     extend: {
+//       colors: {
+//         primary: "#006837",
+//         secondary: "#1d1d1d",
+//         accent: "#198754",
+//         "accent-dark": "#0f5132",
+//       },
+//     },
+//   },
+//   plugins: [],
+// };
