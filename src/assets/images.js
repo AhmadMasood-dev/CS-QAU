bbf69150630fd58a016c618248e7fa4a3301446b
AhmadMasood-dev <ahmadmasood.dev@gmail.com>
2025-04-20 17:52:44 +0500

    assets of images used

---
+++ src/assets/images.js
@@ -0,0 +1,36 @@
+import bg from "./img/bg.jpg"
+import logo from './img/logo.jpeg'
+import qau_logo from './img/qau_logo.png'
+import dept1 from './img/dept1.jpg'
+import dept2 from './img/dept2.jpg'
+import dept3 from './img/dept3.jpg'
+import dept4 from './img/dept4.jpg'
+import dept5 from './img/dept5.jpg'
+import dept6 from './img/dept6.jpg'
+import dept7 from './img/dept7.jpg'
+import dept8 from './img/dept8.jpg'
+import dept9 from './img/dept9.jpg'
+import dept10 from './img/dept10.jpg'
+import dept11 from './img/dept11.jpg'
+import dept12 from './img/dept12.jpg'
+import dept13 from './img/dept13.jpg'
+import dept14 from './img/dept14.jpg'
+import dept15 from './img/dept15.jpg'
+
+import cloud from './img/cloud.jpg'
+import database from './img/database.jpg'
+import network from './img/networks.jpg'
+
+import phd from './img/phd.png'
+import ms from './img/ms.png'
+import msds from './img/ms-ds.png'
+import bs from './img/bs.png'
+import mphil from './img/mphil.png'
+
+import hci from './img/hci.jpg'
+import knowlegeEng from './img/knowlegeEng.jpg'
+import NetworkCom from './img/NetworkCom.jpg'
+
+import deptfac2022 from './img/deptfac2022.jpg'
+
+export { bg, logo, qau_logo, dept1, dept2, dept3, dept4, dept5, dept6, dept7, dept8, dept9, dept10, dept11, dept12, dept13, dept14, dept15, deptfac2022, cloud, database, network, phd, bs, ms, msds, mphil, NetworkCom, hci, knowlegeEng }
