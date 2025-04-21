f7a7b1c5f1d5b607b832c0f25bd227812c33b521
AhmadMasood-dev <ahmadmasood.dev@gmail.com>
2025-04-21 13:47:27 +0500

    add slider and news section

---
+++ src/components/Home/News.jsx
@@ -0,0 +1,79 @@
+function News() {
+  return (
+    <div>
+      <section class="text-white body-font overflow-hidden bg-accent-dark">
+        <div class="container px-5 py-24 mx-auto">
+          <div class="-my-8 divide-y-2 divide-gray-100">
+            <div class="py-8 flex flex-wrap md:flex-nowrap">
+              <div class="md:w-64 md:mb-0 mb-6 flex-shrink-0 flex flex-col">
+                <span class="font-semibold title-font text-gray-300">Date</span>
+                <span class="mt-1 text-gray-200 text-sm">12 Jun 2019</span>
+              </div>
+              <div class="md:flex-grow">
+                <h2 class="text-2xl font-medium text-whites title-font mb-2">
+                  QAU Ranked in QS 2025 for CS & IS
+                </h2>
+                <p class="leading-relaxed">
+                  Quaid-i-Azam University in "Computer Science and Information
+                  Systems" ranked 301-350 worldwide and third nationally by QS
+                  Rankings 2025
+                </p>
+                <a
+                  class="text-indigo-500 inline-flex items-center mt-4"
+                  href="https://www.topuniversities.com/university-subject-rankings/computer-science-information-systems"
+                >
+                  Learn More
+                </a>
+              </div>
+            </div>
+            <div class="py-8 flex flex-wrap md:flex-nowrap">
+              <div class="md:w-64 md:mb-0 mb-6 flex-shrink-0 flex flex-col">
+                <span class="font-semibold title-font text-gray-300">Date</span>
+                <span class="mt-1 text-gray-200 text-sm">12 Jun 2019</span>
+              </div>
+              <div class="md:flex-grow">
+                <h2 class="text-2xl font-medium text-white title-font mb-2">
+                  Seminar by Usman Asif
+                </h2>
+                <p class="leading-relaxed">
+                  Seminar on Entrepreneurial and Growth Dynamics in the Tech
+                  Landscape by Usman Asif, CEO, DevSinc
+                </p>
+                <a
+                  class="text-indigo-500 inline-flex items-center mt-4"
+                  href="https://www.facebook.com/story.php?story_fbid=705090698458911&id=100068739602961"
+                >
+                  Learn More
+                </a>
+              </div>
+            </div>
+            <div class="py-8 flex flex-wrap md:flex-nowrap">
+              <div class="md:w-64 md:mb-0 mb-6 flex-shrink-0 flex flex-col">
+                <span class="font-semibold title-font text-gray-300">Date</span>
+                <span class="text-sm text-gray-200">12 Jun 2019</span>
+              </div>
+              <div class="md:flex-grow">
+                <h2 class="text-2xl font-medium text-white title-font mb-2">
+                  8th ExcITe Cup
+                </h2>
+                <p class="leading-relaxed">
+                  Students from CS Dept got first positions in On Spot
+                  Programming and Hackathon competitions in the 8th ExcITe Cup
+                  organized by CUST, Islamabad
+                </p>
+                <a
+                  class="text-indigo-500 inline-flex items-center mt-4"
+                  href="https://www.topuniversities.com/university-subject-rankings/computer-science-information-systems"
+                >
+                  Learn More
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
+}
+
+export default News;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,17 @@
 import About from "../components/Home/About";
 import Footer from "../components/shared/Footer";
 import Research from "../components/Home/Research";
+import News from "../components/Home/News";
 import NavBar from "../components/shared/Navbar";
+import Slider from "../components/Home/Slider";
 
 function Home() {
   return (
     <>
       <NavBar />
       <About />
+      <News />
+      <Slider />
       <Research />
       <Footer />
     </>
