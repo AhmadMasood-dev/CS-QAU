41b2d4e136c98b22e1cb0f18a4dfc3c10d59a457
AhmadMasood-dev <ahmadmasood.dev@gmail.com>
2025-04-21 13:49:30 +0500

    make faculty page

---
+++ src/pages/Faculty.jsx
@@ -0,0 +1,15 @@
+import Faculties from "../components/Faculty";
+import Footer from "../components/shared/Footer";
+import Navbar from "../components/shared/Navbar";
+
+function Faculty() {
+  return (
+    <div>
+      <Navbar />
+      <Faculties />
+      <Footer />
+    </div>
+  );
+}
+
+export default Faculty;
