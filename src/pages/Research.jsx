6e72fa49e94b1cee78ca22b97479e588255488e4
AhmadMasood-dev <ahmadmasood.dev@gmail.com>
2025-04-21 13:48:48 +0500

    make research page

---
+++ src/pages/Research.jsx
@@ -0,0 +1,14 @@
+import Researchs from "../components/Research";
+import Navbar from "../components/shared/Navbar";
+import Footer from "../components/shared/Footer";
+function Research() {
+  return (
+    <div>
+      <Navbar/>
+      <Researchs />
+      <Footer/>
+    </div>
+  );
+}
+
+export default Research;
